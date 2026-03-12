ed6e82bc820fe79aeb020757a9904addb9b3153b
justin-mcgowan-servaro <justin.mcgowan@servaro.co.za>
2026-03-12 22:55:56 +0200

    fix: CORS for SignalR hub on servaro.co.za

--- TradingIntelligence.Api/Program.cs
+++ TradingIntelligence.Api/Program.cs
@@ -220,7 +220,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins(
+            "http://localhost:4200",
+            "https://servaro.co.za",
+            "https://www.servaro.co.za")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials());
